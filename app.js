20fc8b42748008ea0c353db2f294801d5e719af7
santiec9 <santiagoecheverricolina@gmail.com>
2023-08-28 09:42:13 -0500

    Cambios finales

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ menu.push(new Menu("MARGARITA", 10000));
 
 let cuenta = [];
 alert('Bienvenido a este simulador de facturas de nuestro restaurante =)');
-alert('Para poder disfrutar de toda la experiencia debes de abrir tu consola');
 alert('Ahora si, vamos a comer!!!!')
 while (true) {
     let selector = prompt(`Menú:\n1. QUESADILLA ($30000)\n2. TACOS ($20000)\n3. BURRITO ($40000)\n4. FLAN ($10000)\n5. MARGARITA ($10000)\n\nSelecciona un producto (1-5) o escribe "PAGAR" para finalizar:`);
@@ -55,10 +54,10 @@ for (const item of cuenta) {
 let iva = subtotal * 0.19;
 let total = subtotal + iva;
 
-console.log("Productos seleccionados:");
+document.write("<h1>Productos seleccionados:</h1><br><hr><hr>");
 for (const item of cuenta) {
-    console.log(`${item.plato} x${item.cantidad} - $${item.precio * item.cantidad}`);
+    document.write(`${item.plato} x${item.cantidad} - $${item.precio * item.cantidad}<br><hr>`);
 }
-console.log(`Subtotal: $${subtotal}`);
-console.log(`Total IVA (19%): $${iva}`);
-console.log(`Total a pagar: $${total}`);
+document.write(`Subtotal: $${subtotal}<br><hr>`);
+document.write(`Total IVA (19%): $${iva}<br><hr>`);
+document.write(`Total a pagar: $${total}<br>`);
